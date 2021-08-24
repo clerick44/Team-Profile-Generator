d71cfd0cfe9ec5e3a3d0f0453c76489b8977b0ab
Jesse Erickson <clerick44@hotmail.com>
2021-08-24 11:01:01 -0500

    rebuilt files in /lib to import easier

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const Intern = require("./lib/Intern.js");
 
 const employees = [];
 
+let addMore = "Yes";
+let isMgr = true;
+
 const employeeQuestions = [
   {
     message: "Enter employee's name",
@@ -14,6 +17,18 @@ const employeeQuestions = [
     validate: (answer) => {
       if (!answer) {
         return console.log("Employee name is a required field!");
+      }
+      return true;
+    },
+  },
+  {
+    message: "Enter Manager's office Number.",
+    name: "officeNumber",
+    when: (isMgr) => isMgr,
+    validate: (answer) => {
+      console.log("inq " + isMgr);
+      if (!answer) {
+        return console.log("Office number is a required field!");
       }
       return true;
     },
@@ -45,25 +60,80 @@ const employeeQuestions = [
   },
   {
     type: "list",
-    message: "Select next employee's role",
-    choices: ["None", "Engineer", "Intern", "Manager"],
+    message: "Select employee's role",
+    choices: ["Engineer", "Intern"],
     name: "role",
+    when: (isMgr) => !isMgr,
+  },
+  {
+    message: "Enter Engineer's Github Id.",
+    name: "github",
+    when: (res) => res.role === "Engineer",
+    validate: (answer) => {
+      if (!answer) {
+        return console.log("Github Id is a required field!");
+      }
+      return true;
+    },
+  },
+  {
+    message: "Enter Intern's school.",
+    name: "school",
+    when: (res) => res.role === "Intern",
+    validate: (answer) => {
+      if (!answer) {
+        return console.log("School is a required field!");
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    message: "would you like to add another employee?",
+    choices: ["Yes", "No"],
+    name: "addMore",
   },
 ];
-const managerQuestions = Manager.question.concat(employeeQuestions);
-console.log(managerQuestions);
+// const managerQuestions = Manager.question.concat(employeeQuestions);
 
 function init() {
-  addManager();
+  addEmployee();
 }
 
-async function addManager() {
+//add employee
+async function addEmployee() {
   try {
-    const managerResponse = await inquirer.prompt(managerQuestions);
-    console.log(managerResponse);
-    const { name, id, email, officeNumber } = managerResponse;
-    const newManager = new Manager(name, id, email, officeNumber);
-    console.log(newManager);
+    while (addMore === "Yes") {
+      const response = await inquirer.prompt(employeeQuestions);
+      // console.log(response);
+
+      const { name, id, email, role, github, school, addMore, officeNumber } =
+        response;
+
+      switch (role) {
+        case "Engineer":
+          console.log("engineer");
+          employee = new Engineer(name, id, email, github);
+          employees.push(employee);
+          break;
+
+        case "Intern":
+          console.log("intern");
+          employee = new Intern(name, id, email, school);
+          employees.push(employee);
+          break;
+
+        default:
+          console.log("manager");
+          employee = new Manager(name, id, email, officeNumber);
+          employees.push(employee);
+          isMgr = false;
+          break;
+      }
+      console.log(employees);
+      console.log(addMore);
+      console.log(isMgr);
+    }
   } catch (err) {
     console.log(err);
   }
